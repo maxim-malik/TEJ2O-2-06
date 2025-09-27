d4160cecba56561188b868dd13b11df669c23238
maxim-malik <maxim.malik@stu.ocsb.ca>
2025-09-27 18:38:53 -0400

    added functionallity to output 3.3V to pin 16

--- main.ts
+++ main.ts
@@ -1,8 +1,14 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Max
+ * Created on: Sep 2025
+ * This program turns on and off an LED when a button is pressed
 */
 
-basic.showString('Hello, World!')
+input.onButtonPressed(Button.A, () => { // button A input
+    pins.digitalWritePin(DigitalPin.P16, 1) // turns on 3.3V output from pin 16
+})
+
+input.onButtonPressed(Button.B, () => { // button B input
+    pins.digitalWritePin(DigitalPin.P16, 0) // turns off 3.3V output from pin 16
+})
