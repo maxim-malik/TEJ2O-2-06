99680581f3d1bc8661a94d4fca3f8191b1a11ffd
maxim-malik <maxim.malik@stu.ocsb.ca>
2025-10-21 13:13:39 -0400

    Fixed formatting

--- main.ts
+++ main.ts
@@ -5,10 +5,15 @@
  * This program turns on and off an LED when a button is pressed
 */
 
-input.onButtonPressed(Button.A, () => { // button A input
-    pins.digitalWritePin(DigitalPin.P16, 1) // turns on 3.3V output from pin 16
+// button A input
+input.onButtonPressed(Button.A, () => { 
+    // turns on 3.3V output from pin 16
+    pins.digitalWritePin(DigitalPin.P16, 1)
 })
 
-input.onButtonPressed(Button.B, () => { // button B input
-    pins.digitalWritePin(DigitalPin.P16, 0) // turns off 3.3V output from pin 16
+// button B input
+input.onButtonPressed(Button.B, () => { 
+
+    // turns off 3.3V output from pin 16
+    pins.digitalWritePin(DigitalPin.P16, 0) 
 })
